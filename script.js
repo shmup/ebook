fce5d1c12e39bf700a921b3c9d8a43b561e95aae
Jared Miller <jared@smell.flowers>
2025-04-13 15:12:31 -0400

    Navigate pages with clicks in the 1/3rds

--- script.js
+++ script.js
@@ -27,14 +27,37 @@ function initEpubReader(bookPath) {
   });
 
   rendition.display();
+  const viewer = document.getElementById("viewer");
 
-  document.addEventListener("keyup", function (e) {
-    if (e.keyCode == 37) {
+  function handleNavigation(x) {
+    const width = globalThis.innerWidth;
+    const third = width / 3;
+
+    if (x < third) {
       rendition.prev();
-    }
-    if (e.keyCode == 39) {
+    } else if (x > third * 2) {
       rendition.next();
     }
+  }
+
+  document.addEventListener("keyup", function (e) {
+    if (e.keyCode == 37) rendition.prev();
+    if (e.keyCode == 39) rendition.next();
+  });
+
+  // Note: I was only able to get a perfect click scenario with all 3 handlers
+  rendition.on("click", (e) => {
+    handleNavigation(e.clientX);
+  });
+
+  viewer.addEventListener("click", (e) => {
+    if (e.target !== viewer && viewer.contains(e.target)) return;
+    handleNavigation(e.clientX);
+  });
+
+  document.addEventListener("click", (e) => {
+    if (e.target === viewer || viewer.contains(e.target)) return;
+    handleNavigation(e.clientX);
   });
 }
 
